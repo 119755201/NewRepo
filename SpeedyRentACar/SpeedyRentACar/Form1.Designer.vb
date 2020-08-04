1acb2ffe7775a3db7ed1982c9003b24301c34d50
Corey O Donovan <119755201@umail.ucc.ie>
2020-08-04 17:28:31 +0100

    Updated labels and group box. Applying text box input features next

--- SpeedyRentACar/SpeedyRentACar/Form1.Designer.vb
+++ SpeedyRentACar/SpeedyRentACar/Form1.Designer.vb
@@ -22,9 +22,7 @@ Partial Class frmRentACar
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
@@ -34,89 +32,204 @@ Partial Class frmRentACar
         Me.TextBox8 = New System.Windows.Forms.TextBox()
         Me.TextBox9 = New System.Windows.Forms.TextBox()
         Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.lblCustomerName = New System.Windows.Forms.Label()
+        Me.lblCustomerStAddress = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblCounty = New System.Windows.Forms.Label()
+        Me.lblContactNumber = New System.Windows.Forms.Label()
+        Me.lblPassportNumber = New System.Windows.Forms.Label()
+        Me.lblPaymentType = New System.Windows.Forms.Label()
+        Me.lblCardNumber = New System.Windows.Forms.Label()
+        Me.lblCustomerRequests = New System.Windows.Forms.Label()
+        Me.lblNumberOfDays = New System.Windows.Forms.Label()
+        Me.gbxCustomerInformation = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(262, 101)
+        Me.TextBox1.Location = New System.Drawing.Point(262, 104)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 26)
         Me.TextBox1.TabIndex = 0
         '
-        'ContextMenuStrip1
-        '
-        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
-        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
-        '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(262, 153)
+        Me.TextBox2.Location = New System.Drawing.Point(262, 156)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(100, 26)
         Me.TextBox2.TabIndex = 2
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(262, 199)
+        Me.TextBox3.Location = New System.Drawing.Point(262, 202)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(100, 26)
         Me.TextBox3.TabIndex = 3
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(262, 246)
+        Me.TextBox4.Location = New System.Drawing.Point(262, 249)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(100, 26)
         Me.TextBox4.TabIndex = 4
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(262, 304)
+        Me.TextBox5.Location = New System.Drawing.Point(262, 307)
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.Size = New System.Drawing.Size(100, 26)
         Me.TextBox5.TabIndex = 5
         '
         'TextBox6
         '
-        Me.TextBox6.Location = New System.Drawing.Point(432, 304)
+        Me.TextBox6.Location = New System.Drawing.Point(668, 307)
         Me.TextBox6.Name = "TextBox6"
         Me.TextBox6.Size = New System.Drawing.Size(100, 26)
         Me.TextBox6.TabIndex = 10
         '
         'TextBox7
         '
-        Me.TextBox7.Location = New System.Drawing.Point(432, 246)
+        Me.TextBox7.Location = New System.Drawing.Point(668, 249)
         Me.TextBox7.Name = "TextBox7"
         Me.TextBox7.Size = New System.Drawing.Size(100, 26)
         Me.TextBox7.TabIndex = 9
         '
         'TextBox8
         '
-        Me.TextBox8.Location = New System.Drawing.Point(432, 199)
+        Me.TextBox8.Location = New System.Drawing.Point(668, 202)
         Me.TextBox8.Name = "TextBox8"
         Me.TextBox8.Size = New System.Drawing.Size(100, 26)
         Me.TextBox8.TabIndex = 8
         '
         'TextBox9
         '
-        Me.TextBox9.Location = New System.Drawing.Point(432, 153)
+        Me.TextBox9.Location = New System.Drawing.Point(668, 156)
         Me.TextBox9.Name = "TextBox9"
         Me.TextBox9.Size = New System.Drawing.Size(100, 26)
         Me.TextBox9.TabIndex = 7
         '
         'TextBox10
         '
-        Me.TextBox10.Location = New System.Drawing.Point(432, 101)
+        Me.TextBox10.Location = New System.Drawing.Point(668, 104)
         Me.TextBox10.Name = "TextBox10"
         Me.TextBox10.Size = New System.Drawing.Size(100, 26)
         Me.TextBox10.TabIndex = 6
+        '
+        'lblCustomerName
+        '
+        Me.lblCustomerName.AutoSize = True
+        Me.lblCustomerName.Location = New System.Drawing.Point(131, 104)
+        Me.lblCustomerName.Name = "lblCustomerName"
+        Me.lblCustomerName.Size = New System.Drawing.Size(124, 20)
+        Me.lblCustomerName.TabIndex = 11
+        Me.lblCustomerName.Text = "Customer Name"
+        '
+        'lblCustomerStAddress
+        '
+        Me.lblCustomerStAddress.AutoSize = True
+        Me.lblCustomerStAddress.Location = New System.Drawing.Point(131, 156)
+        Me.lblCustomerStAddress.Name = "lblCustomerStAddress"
+        Me.lblCustomerStAddress.Size = New System.Drawing.Size(116, 20)
+        Me.lblCustomerStAddress.TabIndex = 12
+        Me.lblCustomerStAddress.Text = "Street Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(131, 202)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 20)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Town/City"
+        '
+        'lblCounty
+        '
+        Me.lblCounty.AutoSize = True
+        Me.lblCounty.Location = New System.Drawing.Point(131, 249)
+        Me.lblCounty.Name = "lblCounty"
+        Me.lblCounty.Size = New System.Drawing.Size(59, 20)
+        Me.lblCounty.TabIndex = 14
+        Me.lblCounty.Text = "County"
+        '
+        'lblContactNumber
+        '
+        Me.lblContactNumber.AutoSize = True
+        Me.lblContactNumber.Location = New System.Drawing.Point(122, 310)
+        Me.lblContactNumber.Name = "lblContactNumber"
+        Me.lblContactNumber.Size = New System.Drawing.Size(125, 20)
+        Me.lblContactNumber.TabIndex = 15
+        Me.lblContactNumber.Text = "Contact Number"
+        '
+        'lblPassportNumber
+        '
+        Me.lblPassportNumber.AutoSize = True
+        Me.lblPassportNumber.Location = New System.Drawing.Point(509, 104)
+        Me.lblPassportNumber.Name = "lblPassportNumber"
+        Me.lblPassportNumber.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lblPassportNumber.Size = New System.Drawing.Size(132, 20)
+        Me.lblPassportNumber.TabIndex = 20
+        Me.lblPassportNumber.Text = "Passport Number"
+        '
+        'lblPaymentType
+        '
+        Me.lblPaymentType.AutoSize = True
+        Me.lblPaymentType.Location = New System.Drawing.Point(532, 156)
+        Me.lblPaymentType.Name = "lblPaymentType"
+        Me.lblPaymentType.Size = New System.Drawing.Size(109, 20)
+        Me.lblPaymentType.TabIndex = 19
+        Me.lblPaymentType.Text = "Payment Type"
+        '
+        'lblCardNumber
+        '
+        Me.lblCardNumber.AutoSize = True
+        Me.lblCardNumber.Location = New System.Drawing.Point(450, 202)
+        Me.lblCardNumber.Name = "lblCardNumber"
+        Me.lblCardNumber.Size = New System.Drawing.Size(191, 20)
+        Me.lblCardNumber.TabIndex = 18
+        Me.lblCardNumber.Text = "Credit/Debit Card Number"
+        '
+        'lblCustomerRequests
+        '
+        Me.lblCustomerRequests.AutoSize = True
+        Me.lblCustomerRequests.Location = New System.Drawing.Point(495, 249)
+        Me.lblCustomerRequests.Name = "lblCustomerRequests"
+        Me.lblCustomerRequests.Size = New System.Drawing.Size(155, 20)
+        Me.lblCustomerRequests.TabIndex = 17
+        Me.lblCustomerRequests.Text = "Customer Requests "
+        '
+        'lblNumberOfDays
+        '
+        Me.lblNumberOfDays.AutoSize = True
+        Me.lblNumberOfDays.Location = New System.Drawing.Point(449, 313)
+        Me.lblNumberOfDays.Name = "lblNumberOfDays"
+        Me.lblNumberOfDays.Size = New System.Drawing.Size(192, 20)
+        Me.lblNumberOfDays.TabIndex = 16
+        Me.lblNumberOfDays.Text = "Number of Days Required"
+        '
+        'gbxCustomerInformation
+        '
+        Me.gbxCustomerInformation.Location = New System.Drawing.Point(27, 37)
+        Me.gbxCustomerInformation.Name = "gbxCustomerInformation"
+        Me.gbxCustomerInformation.Size = New System.Drawing.Size(868, 414)
+        Me.gbxCustomerInformation.TabIndex = 22
+        Me.gbxCustomerInformation.TabStop = False
+        Me.gbxCustomerInformation.Text = "Customer Information"
         '
         'frmRentACar
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(934, 577)
+        Me.Controls.Add(Me.lblPassportNumber)
+        Me.Controls.Add(Me.lblPaymentType)
+        Me.Controls.Add(Me.lblCardNumber)
+        Me.Controls.Add(Me.lblCustomerRequests)
+        Me.Controls.Add(Me.lblNumberOfDays)
+        Me.Controls.Add(Me.lblContactNumber)
+        Me.Controls.Add(Me.lblCounty)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblCustomerStAddress)
+        Me.Controls.Add(Me.lblCustomerName)
         Me.Controls.Add(Me.TextBox6)
         Me.Controls.Add(Me.TextBox7)
         Me.Controls.Add(Me.TextBox8)
@@ -127,6 +240,7 @@ Partial Class frmRentACar
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.gbxCustomerInformation)
         Me.Name = "frmRentACar"
         Me.Text = "Rent-A-Car"
         Me.ResumeLayout(False)
@@ -135,7 +249,6 @@ Partial Class frmRentACar
     End Sub
 
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents TextBox4 As TextBox
@@ -145,4 +258,15 @@ Partial Class frmRentACar
     Friend WithEvents TextBox8 As TextBox
     Friend WithEvents TextBox9 As TextBox
     Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents lblCustomerName As Label
+    Friend WithEvents lblCustomerStAddress As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblCounty As Label
+    Friend WithEvents lblContactNumber As Label
+    Friend WithEvents lblPassportNumber As Label
+    Friend WithEvents lblPaymentType As Label
+    Friend WithEvents lblCardNumber As Label
+    Friend WithEvents lblCustomerRequests As Label
+    Friend WithEvents lblNumberOfDays As Label
+    Friend WithEvents gbxCustomerInformation As GroupBox
 End Class
--- SpeedyRentACar/SpeedyRentACar/Form1.vb
+++ SpeedyRentACar/SpeedyRentACar/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmRentACar
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles lblCounty.Click
 
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles lblPassportNumber.Click
+
+    End Sub
 End Class
