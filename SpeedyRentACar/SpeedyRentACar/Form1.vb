baca5d772c7be44dbc5993a6770a9fa858e9c988
Corey O Donovan <119755201@umail.ucc.ie>
2020-08-05 08:06:03 +0100

    Programmed Next and Exit button properties. Form 1 is finished

--- SpeedyRentACar/SpeedyRentACar/Form1.vb
+++ SpeedyRentACar/SpeedyRentACar/Form1.vb
@@ -5,8 +5,21 @@
 
     Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
 
+        ' Shows the next form when the 'Next' button is selected and hides the current form from view '
+
         frmCarInformation.Show()
         Me.Hide()
+
+    End Sub
+
+    Private Sub gbxCustomerInformation_Enter(sender As Object, e As EventArgs) Handles gbxCustomerInformation.Enter
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Closes the program ' 
+
+        Me.Close()
 
     End Sub
 End Class
